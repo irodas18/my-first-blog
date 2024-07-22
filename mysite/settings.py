b02a826e3a1c479154d5c5ee5e2f8d2ebe621191
irodas18 <irrodasherrera2006@elvallecolegio.edu.gt>
2024-07-22 13:14:59 -0600

    Template ensayolab3

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@+d=q$n%=gh^u$zes&k3iedzyjiw(_$&!l91jkj!$gck(8z*pz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
