b2861ff04918297f2f4c68eecfa1a69d2317dd99
irodas18 <irrodasherrera2006@elvallecolegio.edu.gt>
2024-08-19 10:02:00 -0600

    Actualiza configuración y elimina archivos obsoletos

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('nuevo-registro/', views.nuevo_registro, name='nuevo_registro'),
-    path('reportes/', views.generar_reporte, name='generar_reporte'),
-]
